e51a3c4b3149508bad1e1d54302e57102feb5558
David Bomba <turbo124@gmail.com>
2026-01-23 09:20:43 +1100

    Point login route to more efficient query

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -719,7 +719,10 @@ class BaseController extends Controller
             // Set created_at to current time to filter out all existing related records
             // (designs, documents, groups, etc.) for a minimal response payload
             request()->merge(['created_at' => time()]);
-            return $this->miniLoadResponse($query);
+
+            //2026-01-23: Improve Login Performance for react.
+            return $this->refreshReactResponse($query);
+            // return $this->miniLoadResponse($query);
         } 
         elseif ($user->getCompany()->is_large) {
             $this->manager->parseIncludes($this->mini_load);
