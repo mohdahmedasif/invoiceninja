5b8452560eeffed79ef957568618139f4a63d30a
David Bomba <turbo124@gmail.com>
2026-01-28 09:34:57 +1100

    PHP Doc Blocks

--- app/Services/Quickbooks/QuickbooksService.php
+++ app/Services/Quickbooks/QuickbooksService.php
@@ -69,7 +69,7 @@ class QuickbooksService
                 'ClientSecret' => config('services.quickbooks.client_secret'),
                 'auth_mode' => 'oauth2',
                 'scope' => "com.intuit.quickbooks.accounting",
-                'RedirectURI' => $this->testMode ? 'https://qb.romulus.com.au/quickbooks/authorized' : 'https://invoicing.co/quickbooks/authorized',
+                'RedirectURI' => config('services.quickbooks.redirect'),
                 'baseUrl' => $this->testMode ? CoreConstants::SANDBOX_DEVELOPMENT : CoreConstants::QBO_BASEURL,
             ];
 
